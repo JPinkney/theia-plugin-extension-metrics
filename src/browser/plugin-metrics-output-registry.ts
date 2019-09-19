bbac2f9a2535ef3f23ddaa19b89eb0f533eb6c51
Josh Pinkney <joshpinkney@gmail.com>
2019-09-19 14:37:35 -0400

    Add in more supported plugins

--- src/browser/plugin-metrics-output-registry.ts
+++ src/browser/plugin-metrics-output-registry.ts
@@ -26,20 +26,27 @@ export class PluginMetricsOutputChannelRegistry extends OutputChannelRegistryMai
     protected readonly pluginMetricsCreator: PluginMetricsCreator;
 
     // This is a map of output channel names to plugin ids
-    private registryMap = new Map<string, string>();
+    private supportedExtOutputChannelToID = new Map<string, string>();
 
     constructor() {
         super();
-        this.registryMap.set('YAML Support', 'redhat.vscode-yaml');
-        this.registryMap.set('XML Support', 'redhat.vscode-xml');
+        this.supportedExtOutputChannelToID.set('YAML Support', 'redhat.vscode-yaml');
+        this.supportedExtOutputChannelToID.set('XML Support', 'redhat.vscode-xml');
+        this.supportedExtOutputChannelToID.set('Language Support for Apache Camel', 'redhat.vscode-apache-camel');
+        this.supportedExtOutputChannelToID.set('intelephense', 'bmewburn.vscode-intelephense-client');
+        this.supportedExtOutputChannelToID.set('Python', 'ms-python.python');
+        this.supportedExtOutputChannelToID.set('Python Language Server', 'ms-python.python');
+        this.supportedExtOutputChannelToID.set('TypeScript', 'vscode.typescript-language-features');
+        this.supportedExtOutputChannelToID.set('Omnisharp Log', 'theia.omnisharp-theia-plugin');
+        this.supportedExtOutputChannelToID.set('Clang Language Server', 'eclipse-cdt.cdt-vscode');
     }
 
     $append(channelName: string, errorOrValue: string): PromiseLike<void> {
         if (errorOrValue.startsWith('[Error')) {
 
-            if (this.registryMap.has(channelName)) {
+            if (this.supportedExtOutputChannelToID.has(channelName)) {
 
-                const createdMetric = createDefaultRequestData(this.registryMap.get(channelName) as string, errorOrValue);
+                const createdMetric = createDefaultRequestData(this.supportedExtOutputChannelToID.get(channelName) as string, errorOrValue);
                 this.pluginMetricsCreator.createErrorMetric(createdMetric);
 
             } else if (this.pluginMetricsCreator.extensionIDAnalytics.has(channelName)) {
